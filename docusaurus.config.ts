ad023fe857a4135a041acd69fd87ec4fe54561c8
PM3MUENCHEN <pm3munchen@gmail.com>
2024-07-09 20:58:45 +0200

    changed baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://pm3muenchen.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/pm3-wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
