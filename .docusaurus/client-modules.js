bf25af989d1d7166d0d8d4fb53d7080cb8fae78d
PM3MUENCHEN <pm3munchen@gmail.com>
2024-07-10 10:20:31 +0200

    remove .docusaurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\handy\\Projects\\PM3\\Wiki\\pm3-wiki\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\handy\\Projects\\PM3\\Wiki\\pm3-wiki\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\handy\\Projects\\PM3\\Wiki\\pm3-wiki\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\handy\\Projects\\PM3\\Wiki\\pm3-wiki\\src\\css\\custom.css"),
-];
